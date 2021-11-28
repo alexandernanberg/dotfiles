26e5183a633ec9c9098d3b889e978eca7caf231f
Alexander Nanberg <alex.nanberg@hotmail.com>
2021-11-28 11:09:49 +0100

    chore: update brew install

--- brew.sh
+++ brew.sh
@@ -5,7 +5,7 @@ printf "\nInstalling Homebrew packages..."
 
 # Check for Homebrew Installation
 if ! which brew > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi;
 
 # Update Homebrew
