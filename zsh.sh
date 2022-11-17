9616225acf3d412c6c07380e30e7f2b1ef6af1b2
Alexander Nanberg <alex.nanberg@hotmail.com>
2022-11-17 15:16:07 +0100

    fix(zsh): make installer work without SSH

--- zsh.sh
+++ zsh.sh
@@ -14,29 +14,29 @@ fi;
 printf "\nInstalling plugins...\n"
 
 printf "> zsh-syntax-highlighting "
-if [ -d "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
-  git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 else
   git -C ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull
 fi;
 
 printf "> zsh-autosuggestions "
-if [ -d "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-  git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 else
   git -C ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull
 fi;
 
 printf "> zsh-nvm "
-if [ -d "~/.oh-my-zsh/custom/plugins/zsh-nvm" ]; then
-  git clone git@github.com:lukechilds/zsh-nvm.git ~/.oh-my-zsh/custom/plugins/zsh-nvm
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-nvm" ]; then
+  git clone https://github.com/lukechilds/zsh-nvm.git ~/.oh-my-zsh/custom/plugins/zsh-nvm
 else
   git -C ~/.oh-my-zsh/custom/plugins/zsh-nvm pull
 fi;
 
 printf "> pure "
-if [ -d "~/.oh-my-zsh/custom/plugins/pure" ]; then
-  git clone git@github.com:sindresorhus/pure.git ~/.oh-my-zsh/custom/plugins/pure
+if [ ! -d "~/.oh-my-zsh/custom/plugins/pure" ]; then
+  git clone https://github.com/sindresorhus/pure.git ~/.oh-my-zsh/custom/plugins/pure
 else
   git -C ~/.oh-my-zsh/custom/plugins/pure pull
 fi;
